815bdb8576f93a062da804dba23755638c54be73
nigolarer <pinecone@qq.com>
2017-10-16 02:03:49 +0800

    original Java Version ,less comments ,not reedit

--- CodingInterviewJava/Chapter5/longestSubstringWithoutDuplication_48.java
+++ CodingInterviewJava/Chapter5/longestSubstringWithoutDuplication_48.java
@@ -31,8 +31,10 @@ public class longestSubstringWithoutDuplication_48 {
     }
     /*
     * 首先,用来那个个变量,一个是动态记录当前的最大子串的长度,另一个是记录历史最大子串的长度
-    * 其次,定义了一个计算当前字符上次出现的位置的函数,这里并没有再申请空间,与书上不同.这里
-    * 如果字符串很长很长那么定义一个辅助空间更划算,我这边遍历的话还是容易O(n^2)
+    * 其次,定义了一个计算当前字符上次出现的位置的函数,这里并没有再申请空间,与书上不同.
+    * 如果字符串很长很长那么定义一个辅助空间更划算,我这边getLastPos遍历的话还是容易O(n^2)
+    * 修改getLastPos函数后的checkLastPos,申请了额外空间
+    * 就可以很轻松的实现辅助空间版本,这里不再赘述.
     * 接下来,如果返回结果是-1(表明该字符之前没有在字符串中出现)或者长度大于当前最大长度(curLen),则当前最大长度自增1
     * 如果返回结果是小于当前最大长度,则覆盖当前最大长度值
     * 每一趟下来都与历史最大长度比较如果大于历史最大长度则更新它最后返回最大值
@@ -44,8 +46,13 @@ public class longestSubstringWithoutDuplication_48 {
         if (charS == null) {//非法判定
             return maxLen;
         }
+        int[] position = new int[26];//这里申请额外空间来存储最后一次出现该元素的位置
+        for(int i = 0;i<position.length;i++) {
+            position[i] = -1;
+        }
         for (int i = 0; i < charS.length; i++) {
-            int lastPos = getLastPos(charS, i);
+            //int lastPos = getLastPos(charS, i);
+            int lastPos = checkLastPos(charS,position,i,1);
             if (lastPos < 0 || i - lastPos > curLen) {//注意这里是大于curLen而非maxLen
                 curLen++;
             }
@@ -55,10 +62,13 @@ public class longestSubstringWithoutDuplication_48 {
             if (maxLen < curLen) {
                 maxLen = curLen;
             }
+            checkLastPos(charS,position,i,0);//更新最后一次该元素位置
         }
         return maxLen;
     }
-
+    /*
+    * 不申请辅助空间版
+    * */
     private static int getLastPos(char[] charS, int pos) {
         int lastPos = -1;
         for(int i = 0;i<pos;i++) {
@@ -67,5 +77,19 @@ public class longestSubstringWithoutDuplication_48 {
             }
         }
         return lastPos;
+    }
+    /*
+    * 辅助空间版本的lastPos,direction为1时作为查询,返回lastpos
+    * direction不为1时,作为更新position的lastpos
+    *
+    * */
+    private static int checkLastPos(char[] charS, int[] position,int pos,int direction) {
+        if (direction == 1) {
+            return position[charS[pos]-'a'];
+        } else {
+            position[charS[pos]-'a'] = pos;
+            return 0;
+        }
+
     }
 }
