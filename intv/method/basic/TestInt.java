8bda6516d8529f0352a176e97bdb6dfb31b883cc
nigolarer <pinecone@qq.com>
2017-10-29 17:17:26 +0800

    updated some method of bi-tree

---
+++ intv/method/basic/TestInt.java
@@ -0,0 +1,17 @@
+package intv.method.basic;
+
+/**
+ * @author nigolarer
+ * 测试int一些基础
+ * int的除法是向下取整
+ * */
+public class TestInt {
+    public static void main(String[] args) {
+        //i == 7
+        int i = 15/2;
+        System.out.println(i);
+        //j == 11
+        int j = 23/2;
+        System.out.println(j);
+    }
+}
